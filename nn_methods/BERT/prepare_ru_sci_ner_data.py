e821fa852745e9ada8f673ca794bbf76fecfe07a
AlekseyPauls <aleksey.pauls@mail.ru>
2020-12-14 13:11:03 +0700

    Add support of original markup in .xlsx format

--- nn_methods/BERT/prepare_ru_sci_ner_data.py
+++ nn_methods/BERT/prepare_ru_sci_ner_data.py
@@ -7,19 +7,23 @@ import random
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', default="./")
-    parser.add_argument('--output_dir', default="./")
-    parser.add_argument('--test_split', default=0.0, type=float)
+    parser.add_argument('--input_dir', default="./preprocessed")
+    parser.add_argument('--output_dir', default="./ner_data")
+    parser.add_argument('--test_split', default=0.2, type=float)
+    parser.add_argument('--seed', default=42, type=float)
 
     args = parser.parse_args(sys.argv[1:])
 
-    with open(os.path.join(args.output_dir, "train.txt"), "w") as train_out, \
-         open(os.path.join(args.output_dir, "dev.txt"), "w") as test_out:
+    with open(os.path.join(args.output_dir, "train.txt"), "w", encoding="utf-8") as train_out, \
+         open(os.path.join(args.output_dir, "dev.txt"), "w", encoding="utf-8") as test_out:
         examples = []
         for f_name in os.listdir(args.input_dir):
-            #print(f_name)
-            df = pd.read_csv(os.path.join(args.input_dir, f_name), delimiter="\t")
-            queries = df[["token", "Лена"]].dropna().values
+            if f_name == "relations.csv":
+                continue
+
+            print(f_name)
+            df = pd.read_csv(os.path.join(args.input_dir, f_name))
+            queries = df[["token", "reviewed"]].dropna().values
             example = []
             for q in queries:
                example.append(q)
@@ -29,6 +33,7 @@ if __name__ == "__main__":
             if len(example) > 0:
                 examples.append(example)
 
+        random.seed(args.seed)
         for ex in examples:
             r = random.random()
             if r < args.test_split:
